ea06ec3d7c98d02acd811b2a7dd8e8101fa1c9da
Mahati Gollamudi <github email address>
2016-09-26 13:30:08 -0400

    Add event handlers and bound this

---
+++ components/Keypad.js
@@ -0,0 +1,19 @@
+const React = require('react');
+class Keypad extends React.Component {
+  constructor() {
+    super();
+
+    this.typing = this.typing.bind(this);
+  }
+  typing() {
+    console.log('Entering password...');
+  }
+  render() {
+    return (
+      <div>
+        <input type="password" onKeyUp={this.typing}></input>
+      </div>
+    );
+  }
+}
+module.exports = Keypad
